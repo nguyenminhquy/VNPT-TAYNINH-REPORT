09e9b491f019a533c1b27c29700261e6d92166fa
nguyenminhquy <quy.nguyen050104@gmail.com>
2026-07-21 23:10:14 +0700

    Configure Next.js rewrites for Vercel Python standalone api routing

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {},
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/export_python',
+        destination: '/api/export',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
