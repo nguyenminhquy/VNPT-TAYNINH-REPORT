76b52d8b26f3e8fdc0a434c346f2e8951067e903
nguyenminhquy <quy.nguyen050104@gmail.com>
2026-07-21 23:41:06 +0700

    Update logo and fix bg cache

--- webapp/components/VnptLogo.tsx
+++ webapp/components/VnptLogo.tsx
@@ -2,25 +2,11 @@ import React from 'react';
 
 export default function VnptLogo({ className = "", style = {} }: { className?: string; style?: React.CSSProperties }) {
   return (
-    <svg 
+    <img 
+      src="/vnpt-logo-new.png" 
+      alt="VNPT Logo" 
       className={className} 
       style={style} 
-      viewBox="0 0 200 60" 
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <text 
-        x="0" 
-        y="45" 
-        fontFamily="'Arial', 'Helvetica', sans-serif" 
-        fontWeight="900" 
-        fontStyle="italic"
-        fontSize="48" 
-        fill="#005BAA"
-        letterSpacing="-1.5"
-      >
-        VNPT
-      </text>
-    </svg>
+    />
   );
 }
