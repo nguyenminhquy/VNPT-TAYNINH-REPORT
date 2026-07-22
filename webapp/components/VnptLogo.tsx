d8c168497fde787e9c0a6fce3a8f6a1bc42fcc91
nguyenminhquy <quy.nguyen050104@gmail.com>
2026-07-22 07:35:03 +0700

    Disable eslint for img tag

--- webapp/components/VnptLogo.tsx
+++ webapp/components/VnptLogo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default function VnptLogo({ className = "", style = {} }: { className?: string; style?: React.CSSProperties }) {
   return (
+    // eslint-disable-next-line @next/next/no-img-element
     <img 
       src="/vnpt-logo-new.png" 
       alt="VNPT Logo" 
