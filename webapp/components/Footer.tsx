c7f52ca3963e809a4651cf38711ddc69a2ab2c08
nguyenminhquy <quy.nguyen050104@gmail.com>
2026-07-26 17:24:44 +0700

    fix(build): fix lucide imports and @import positioning for clean production build of official VNPT footer

--- webapp/components/Footer.tsx
+++ webapp/components/Footer.tsx
@@ -2,12 +2,22 @@
 
 import React from 'react';
 import { 
-  Facebook, 
-  Youtube, 
   Globe, 
   ChevronUp 
 } from 'lucide-react';
 import './Footer.css';
+
+const FacebookIcon = () => (
+  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047v-2.66c0-3.025 1.792-4.697 4.533-4.697 1.312 0 2.686.236 2.686.236v2.971H15.83c-1.491 0-1.956.93-1.956 1.886v2.264h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z"/>
+  </svg>
+);
+
+const YoutubeIcon = () => (
+  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+  </svg>
+);
 
 export default function Footer() {
   const scrollToTop = () => {
@@ -110,7 +120,7 @@ export default function Footer() {
               className="social-circle-btn fb"
               title="Facebook VNPT Tây Ninh"
             >
-              <Facebook size={20} fill="currentColor" />
+              <FacebookIcon />
             </a>
             <a 
               href="https://youtube.com/@vnpttayninh" 
@@ -119,7 +129,7 @@ export default function Footer() {
               className="social-circle-btn yt"
               title="YouTube VNPT Tây Ninh"
             >
-              <Youtube size={20} fill="currentColor" />
+              <YoutubeIcon />
             </a>
             <a 
               href="https://vnpttayninh.vn" 
