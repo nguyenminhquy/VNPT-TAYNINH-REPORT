20083e32e618df221d9a1a7faf26eb5130bd0de4
nguyenminhquy <quy.nguyen050104@gmail.com>
2026-08-09 13:44:29 +0700

    Explicitly pass token to handleUpload

--- webapp/app/api/upload-token/route.ts
+++ webapp/app/api/upload-token/route.ts
@@ -8,6 +8,7 @@ export async function POST(request: Request): Promise<NextResponse> {
     const jsonResponse = await handleUpload({
       body,
       request,
+      token: process.env.BLOB_READ_WRITE_TOKEN,
       onBeforeGenerateToken: async (pathname) => {
         return {
           maximumSizeInBytes: 50 * 1024 * 1024, // 50MB
